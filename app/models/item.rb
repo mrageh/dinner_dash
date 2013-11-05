5c28b3bd03a51df7beaf1b6809e86ec79d5eb304
Luke Martinez <lukemartinez@gmail.com>
2013-11-05 16:18:13 -0700

    changed validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :category
-  validates :description, presence: true
-  validates :name, presence: true
+  validates :description, :name, presence: true
   validates :price, numericality: { greater_than: 0}
 end
