41bb0d7cedfd6f6c7c38930232d3674e99a09dd4
Luke Martinez <lukemartinez@gmail.com>
2013-11-05 16:16:15 -0700

    added some validation, and played with stylesheet

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ActiveRecord::Base
   belongs_to :category
   validates :description, presence: true
   validates :name, presence: true
+  validates :price, numericality: { greater_than: 0}
 end
