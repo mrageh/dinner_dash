574fb963a63d1e3e455f5d0ffdd6d58a6e848129
Adam89 <Adamm1@aston.ac.uk>
2013-11-04 17:48:25 -0700

    so name and description columns cannot be nil

--- db/migrate/20131105003314_create_items.rb
+++ db/migrate/20131105003314_create_items.rb
@@ -1,11 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.string       :name
-      t.text         :description
+      t.string       :name,        :null => false
+      t.text         :description, :null => false
       t.decimal      :price
-      t.timestamps
-      t.references   :category, index: true
+      # t.references   :category, index: true
 
       t.timestamps
     end
