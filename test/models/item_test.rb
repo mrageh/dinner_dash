e845e288838c07e463794683073a46d205515203
Adam89 <Adamm1@aston.ac.uk>
2013-11-05 13:00:59 -0700

    changed our tests to use fixtures

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  
+
   test "should not save item without description" do
-    item = Item.new(name: "taco")
+    item = items(:three)
     refute item.save
   end
 
   test "should not save item without name" do
-    item = Item.new(description: "spicy taco")
+    item = items(:three)
     refute item.save
   end
 end
