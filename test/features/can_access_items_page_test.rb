5b042e2d158e0cb21be9a4f38973d0d39a7728b8
Adam89 <Adamm1@aston.ac.uk>
2013-11-05 20:14:35 -0700

    created a new view and added controller methods

--- test/features/can_access_items_page_test.rb
+++ test/features/can_access_items_page_test.rb
@@ -3,6 +3,9 @@ require "test_helper"
 class CanAccessItemsPageTest < Capybara::Rails::TestCase
   def test_it_views_items_page
     visit items_path
-    assert  page.has_content?("All items"g)
+    click_link "New Item"
+    fill_in "title", :with => "YO MAMA!"ra
+    click_on "Save Item"
+    assert page.has_content?("YO MAMA!")
   end
 end
