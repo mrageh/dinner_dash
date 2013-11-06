fde0c678d76a3e394355d575e5bc00ff036d05e6
Luke Martinez <lukemartinez@gmail.com>
2013-11-06 11:09:17 -0700

    db seed set up with faker, and menu test was updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+class Seeder
+  def initialize
+    create_items
+  end
+
+  def create_items
+    10.times do
+      item = create_item
+      puts "Creating Item #{item.name}"
+    end
+  end
+
+  def create_item
+    item = Item.new
+    item.name = Faker::Lorem.word
+    item.description = Faker::Lorem.sentence
+    item.price = Random.rand(10..100)
+    item.save
+    item
+  end
+end
+
+Seeder.new
--- test/features/can_access_menu_test.rb
+++ test/features/can_access_menu_test.rb
@@ -7,14 +7,15 @@ class CanAccessMenuTest < Capybara::Rails::TestCase
   end
 
   def test_it_shows_categories
+    Category.create(name: "Beverages")
+
     visit categories_path
-    # within "#cat" do
-    # puts category.name
-      assert_selector 'li:first', text: "Sandwiches"
-      page.must_have_selector 'li:first', text: "Sandwiches"
-    # end
-    # assert page.has_content?("Beverages")
-    # assert page.has_content?("Baked Goods")
-    # assert page.has_content?("Sides")
+    save_and_open_page
+    within "#cat" do
+      assert page.has_content?("Beverages")
+    end
+
+     # assert_selector 'li:first', text: "Sandwiches"
+     #  page.must_have_selector 'li:first', text: "Sandwiches"
   end
 end
