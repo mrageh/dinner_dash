8a0c1049c808ee5ff535bbc57d26ec591419b2b9
Luke Martinez <lukemartinez@gmail.com>
2013-11-07 11:54:46 -0700

    links to cart and to menu working

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,7 @@ class OrdersController < ApplicationController
 
   def index
     @orders = Order.all
+    @items = @orders.first.items.first.name
   end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,8 @@ class Order < ActiveRecord::Base
   has_many :order_items
   has_many :items, through: :order_items
 
+  def order_names_and_description
+    order.items.all
+  end
+
 end
