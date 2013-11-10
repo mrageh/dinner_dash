43806f3d9648dd81c7f01c39e11aed5ddde310aa
Adam89 <Adamm1@aston.ac.uk>
2013-11-09 17:34:11 -0700

    Added admin column to the table and set it to default also fixed item style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131108070555) do
+ActiveRecord::Schema.define(version: 20131110002412) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20131108070555) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
